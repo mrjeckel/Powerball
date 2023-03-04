bd076a62d8578b58e8676e252ce4d2369bef7364
Jonathan Bogasky <boganaut@gmail.com>
2023-03-04 15:28:54 -0600

    add test data file

---
+++ test/test.py
@@ -0,0 +1,15 @@
+"""
+ball
+-----------------------------------
+69 has never been drawn
+1 has been drawn 8 times
+2, 3, 4, 5 have been drawn 5 times
+65, 66, 67, 68 have been drawn once
+everything else 1-69 has been drawn twice
+
+powerball
+--------------------------------
+26 has never been drawn
+1 has been drawn 3 times
+everything else 1-29 has been drawn once
+"""
